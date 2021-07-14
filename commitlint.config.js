5db409215ddb7d8160a5106a043890942eaf8c3a
Sharvil Patel <sharvil.patel@capco.com>
2021-07-14 15:26:07 -0400

    installed packages

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+const { rules } = require("@commitlint/config-conventional");
+
+module.exports = {
+    extends: [
+        '@commitlint/config-conventional'
+    ],
+    rules: {
+        //0-disables the rules, 1-warning, 2-error
+        
+    }
+}
